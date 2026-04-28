122081e0faa9e53781734aac3f415f28b9be1a6c
vishalshahh <vishal12kl@gmail.com>
2026-04-28 15:59:02 +0530

    feat: added update, alter. truncate practice questions

--- Day-1-Activity/day-1.sql
+++ Day-1-Activity/day-1.sql
@@ -33,91 +33,145 @@ CREATE TABLE Orders (
 
 -- Insert Customers Data
 INSERT INTO Customers VALUES
-(1, 'Vishal', 'hi@vishy.dev', 'Uttar Pradesh'),
-(2, 'Aaysha', 'aaysha@gmail.com', 'Madhya Pradesh'),
-(3, 'Aditya', 'aditya@gmail.com', 'Pakistan');
+(1,'Vishal','hi@vishy.dev','Uttar Pradesh'),
+(2,'Aaysha','aaysha@gmail.com','Madhya Pradesh'),
+(3,'Aditya','aditya@gmail.com','Pakistan');
 
 -- Insert Restaurants Data
 INSERT INTO Restaurants VALUES
-(101, 'Pizza Hut', 'Italian', 'Ghaziabad'),
-(102, 'Dominos', 'Farm Fresh', 'Bhopal'),
-(103, 'Burger Singh', 'Burger', 'Lahore');
+(101,'Pizza Hut','Italian','Ghaziabad'),
+(102,'Dominos','Farm Fresh','Bhopal'),
+(103,'Burger Singh','Burger','Lahore');
 
 -- Insert Orders Data
 INSERT INTO Orders VALUES
-(1001, 1, 102, 450.00, '2026-04-28'),
-(1002, 2, 101, 799.00, '2026-04-28'),
-(1003, 3, 103, 1200.00, '2026-04-28');
+(1001,1,102,450.00,'2026-04-28'),
+(1002,2,101,799.00,'2026-04-28'),
+(1003,3,103,1200.00,'2026-04-28');
 
 -- View Data
 SELECT * FROM Customers;
 SELECT * FROM Restaurants;
 SELECT * FROM Orders;
 
--- Use Database
-USE zomatodb;
-
 -- PART 1 : SELECT QUERIES
 
--- Use Case 1: View all customers
 SELECT * FROM Customers;
 
--- Use Case 2: Customers from Delhi
 SELECT * FROM Customers
-WHERE city = 'Pakistan';
+WHERE city='Pakistan';
 
--- Use Case 3: High-value salary (order amount above 60000)
 SELECT * FROM Orders
 WHERE order_amount > 100;
 
--- Use Case 4: Orders placed on specific date
 SELECT * FROM Orders
-WHERE order_date = '2026-04-28';
+WHERE order_date='2026-04-28';
 
--- Use Case 5: Sort orders by highest amount
 SELECT * FROM Orders
 ORDER BY order_amount DESC;
 
--- Use Case 6: Top 2 highest orders
 SELECT * FROM Orders
 ORDER BY order_amount DESC
 LIMIT 2;
 
-
 -- PART 2 : UPDATE QUERIES
 
--- Use Case 7: Ravi moved Delhi to Pune
 UPDATE Customers
-SET city = 'Delhi'
-WHERE name = 'Aditya';
+SET city='Delhi'
+WHERE name='Aditya';
 
--- Use Case 8: Pizza World now Fast Food
 UPDATE Restaurants
-SET cuisine_type = 'Italian'
-WHERE name = 'Pizza Hut';
+SET cuisine_type='Italian'
+WHERE name='Pizza Hut';
 
--- Use Case 9: Order 1001 amount corrected
 UPDATE Orders
-SET order_amount = 550
-WHERE order_id = 1001;
-
+SET order_amount=550
+WHERE order_id=1001;
 
 -- PART 3 : DELETE QUERIES
 
--- Use Case 10: Delete customer ID 3
 DELETE FROM Customers
-WHERE customer_id = 3;
+WHERE customer_id=3;
 
--- Use Case 11: Cancel order 1002
 DELETE FROM Orders
-WHERE order_id = 1001;
+WHERE order_id=1001;
 
--- Use Case 12: Remove restaurants from Bangalore
 DELETE FROM Restaurants
-WHERE city = 'Bhopal';
+WHERE city='Bhopal';
 
-
--- Always check before delete
 SELECT * FROM Customers;
 SELECT * FROM Orders;
 SELECT * FROM Restaurants;
+
+-- Update data
+UPDATE Customers
+SET city='Ghaziabad'
+WHERE customer_id=1;
+
+-- Parameterized Query (JDBC)
+-- UPDATE Customers SET city=? WHERE customer_id=?;
+
+-- Delete data
+DELETE FROM Customers
+WHERE customer_id=2;
+
+-- Parameterized Query (JDBC)
+-- DELETE FROM Customers WHERE customer_id=?;
+
+-- Sorting Data
+SELECT * FROM Orders
+ORDER BY order_amount DESC;
+
+-- Top 2 highest orders
+SELECT * FROM Orders
+ORDER BY order_amount DESC
+LIMIT 2;
+
+-- 2nd highest order
+SELECT * FROM Orders
+ORDER BY order_amount DESC
+LIMIT 1 OFFSET 1;
+
+-- Difference between DROP / TRUNCATE / DELETE
+
+CREATE TABLE temp_users (
+id INT,
+username VARCHAR(30),
+role_name VARCHAR(30)
+);
+
+INSERT INTO temp_users VALUES
+(1,'A','Admin'),
+(2,'B','User');
+
+SELECT * FROM temp_users;
+
+DELETE FROM temp_users WHERE id=2;
+
+TRUNCATE TABLE temp_users;
+
+DROP TABLE temp_users;
+
+-- ALTER Commands
+
+-- Add new column
+ALTER TABLE Customers
+ADD mobile VARCHAR(15);
+
+-- Modify column
+ALTER TABLE Customers
+MODIFY mobile VARCHAR(20);
+
+-- Rename column
+ALTER TABLE Customers
+RENAME COLUMN mobile TO phone_number;
+
+-- Drop column
+ALTER TABLE Customers
+DROP COLUMN phone_number;
+
+-- Rename table
+RENAME TABLE Customers TO Customer_Data;
+
+-- View final structure
+DESC Customer_Data;
